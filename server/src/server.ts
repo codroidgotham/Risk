d91a770f0c884c67502d9f7efdc7b8abc55fe599
codroidgotham <no.one1970@protonmail.com>
2023-01-17 22:46:01 +0500

    one post route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,9 @@
 import * as express from 'express'
 import * as mongoDB from 'mongodb'
 
+import bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json())
 app.route("/").get((req: express.Request, res: express.Response) => {
 
     (async function run() {
@@ -10,6 +12,25 @@ app.route("/").get((req: express.Request, res: express.Response) => {
         const db: mongoDB.Db = client.db("RAdatabase")
         const col: mongoDB.Collection = db.collection("Classes")
         const result = await col.find({}).toArray()
+        console.log("serviced lll request at", req.baseUrl)
+        client.close();
+        res.send(result)
+    })().catch((err) => console.log)
+
+
+})
+app.route("/classes").post((req: express.Request, res: express.Response) => {
+
+    const {Name,classId,Confidentiality,Integrity,Availability}=req.body;
+    const obj={"Name":Name,"classId":classId,"Confidentiality":Confidentiality,"Integrity":Integrity,"Availability":Availability};
+    
+   
+    (async function run() {
+        const client = new mongoDB.MongoClient("mongodb://127.0.0.1:27017");
+        await client.connect()
+        const db: mongoDB.Db = client.db("RAdatabase")
+        const col: mongoDB.Collection = db.collection("Classes")
+        const result = await col.insertOne(obj);
         console.log("serviced lll request at", req.baseUrl)
         client.close();
         res.send(result)
