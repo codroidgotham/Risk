4546e755a2c676e6d23fcc77aeb284ee6eaaf6b4
codroidgotham <no.one1970@protonmail.com>
2023-01-18 14:19:00 +0500

    server working

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,23 +1,29 @@
 import * as express from 'express'
 import * as mongoDB from 'mongodb'
-
+const cors=require('cors')
 import bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json())
+app.use(cors())
 app.route("/").get((req: express.Request, res: express.Response) => {
+    
+    console.log("getting");
 
     (async function run() {
+        console.log("inside");
         const client = new mongoDB.MongoClient("mongodb://127.0.0.1:27017");
         await client.connect()
+        console.log("connected");
         const db: mongoDB.Db = client.db("RAdatabase")
         const col: mongoDB.Collection = db.collection("Classes")
         const result = await col.find({}).toArray()
         console.log("serviced lll request at", req.baseUrl)
         client.close();
+        
         res.send(result)
     })().catch((err) => console.log)
 
-
+    
 })
 app.route("/classes").post((req: express.Request, res: express.Response) => {
 
